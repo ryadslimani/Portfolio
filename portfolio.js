4ad848777bfe09697d5a29fcee280072edbc09c6
Ryad SLIMANI <slimanir.ryad@gmail.com>
2026-09-08 14:35:06 +0200

    feat: initial commit with custom portfolio filenames

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+// Mise à jour automatique de l'année dans le footer
+document.addEventListener("DOMContentLoaded", () => {
+    const yearSpan = document.getElementById("current-year");
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
