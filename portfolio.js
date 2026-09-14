1f1288cfd7aeee70847ac33c82cb5d6cc520b0c2
Ryad SLIMANI <slimanir.ryad@gmail.com>
2026-09-14 13:50:17 +0200

    feat: Changement de thème et nettoyage CSS

--- portfolio.js
+++ portfolio.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- 2. Effet de frappe (Typing Effect) dans la Hero Section ---
     const typedTextEl = document.getElementById('typed-text');
     const textsToType = [
-        "Passionné de Sécurité des Systèmes & Réseaux",
+        "Sécurité des Systèmes & Réseaux",
         "Développeur Full-Stack & Scripts Python",
         "Créateur de Solutions Numériques"
     ];
